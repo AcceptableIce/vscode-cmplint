b41724246e4ac33512a7007b776806f3ac6f4115
May Roussel <may@maybreak.com>
2017-04-10 11:06:38 -0700

    Initial commit.

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
-'use strict';
+"use strict";
 
-const langServer = require('vscode-languageserver');
+const langServer = require("vscode-languageserver");
 const Files = langServer.Files;
-const stylelintVSCode = require('stylelint-vscode');
+const cmplintServerWrapper = require("./cmplintServerWrapper");
+const path = require("path");
 
 let config;
 let configOverrides;
@@ -10,73 +11,76 @@ let configOverrides;
 const connection = langServer.createConnection(process.stdin, process.stdout);
 const documents = new langServer.TextDocuments();
 
-const supportedCustomSyntaxes = new Set(['less', 'scss']);
+const supportedCustomSyntaxes = new Set(["cmp", "js"]);
 
 function validate(document) {
-  const options = {code: document.getText()};
+	const options = {
+		code: document.getText()
+	};
 
-  const filePath = Files.uriToFilePath(document.uri);
+	const filePath = Files.uriToFilePath(document.uri);
 
-  if (filePath) {
-    options.codeFilename = filePath;
-  }
+	options.directory = path.join(filePath, "..");
+	options.filter = filePath;
 
-  if (config) {
-    options.config = config;
-  }
+	if(filePath) {
+		options.codeFilename = filePath;
+	}
 
-  if (configOverrides) {
-    options.configOverrides = configOverrides;
-  }
+	if(config) {
+		options.config = config;
+	}
 
-  if (supportedCustomSyntaxes.has(document.languageId)) {
-    options.syntax = document.languageId;
-  }
+	if(configOverrides) {
+		options.configOverrides = configOverrides;
+	}
 
-  return stylelintVSCode(options).then(diagnostics => {
-    connection.sendDiagnostics({uri: document.uri, diagnostics});
-  }).catch(err => {
-    if (err.reasons) {
-      err.reasons.forEach(reason => connection.window.showErrorMessage('stylelint: ' + reason));
-      return;
-    }
+	if(supportedCustomSyntaxes.has(document.languageId)) {
+		options.syntax = document.languageId;
+	}
 
-    // https://github.com/stylelint/stylelint/blob/4.2.0/src/utils/configurationError.js#L3
-    if (err.code === 78) {
-      connection.window.showErrorMessage('stylelint: ' + err.message);
-      return;
-    }
-
-    connection.window.showErrorMessage(err.stack.replace(/\n/g, ' '));
-  });
+	return cmplintServerWrapper(options).then(diagnostics => {
+		connection.sendDiagnostics({
+			uri: document.uri,
+			diagnostics
+		});
+	}).catch(err => {
+		if(err.reasons) {
+			err.reasons.forEach(reason => connection.window.showErrorMessage("cmplint: " + reason));
+			
+			return;
+		}
+		connection.window.showErrorMessage(err.stack.replace(/\n/g, " "));
+	});
 }
 
 function validateAll() {
-  return Promise.all(documents.all().map(document => validate(document)));
+	return Promise.all(documents.all().map(document => validate(document)));
 }
 
 connection.onInitialize(() => {
-  validateAll();
+	validateAll();
 
-  return {
-    capabilities: {
-      textDocumentSync: documents.syncKind
-    }
-  };
+	return {
+		capabilities: {
+			textDocumentSync: documents.syncKind
+		}
+	};
 });
 connection.onDidChangeConfiguration(params => {
-  const {settings} = params;
-  config = settings.stylelint.config;
-  configOverrides = settings.stylelint.configOverrides;
+	const { settings } = params;
 
-  validateAll();
+	config = settings.stylelint.config;
+	configOverrides = settings.stylelint.configOverrides;
+
+	validateAll();
 });
 connection.onDidChangeWatchedFiles(() => validateAll());
 
 documents.onDidChangeContent(event => validate(event.document));
 documents.onDidClose(event => connection.sendDiagnostics({
-  uri: event.document.uri,
-  diagnostics: []
+	uri: event.document.uri,
+	diagnostics: []
 }));
 documents.listen(connection);
 
